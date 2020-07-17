5c6b2f9079471a5e3d72c3960c86c1a6c0dcf6c9
Takayuki.Inoue <takayuki.inoue@i-studio.co.jp>
2020-07-17 13:17:25 +0900

    scrollaction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+class App extends React.Component {
+  state = {
+    scrollTop: 0,
+    pagePos: false
+  }
+  componentDidMount(){
+    console.log(document.getElementById("text").getBoundingClientRect(),document.getElementById("text").clientHeight);
+    document.body.style.overflow = "hidden";
+    document.getElementById("text").style.height = `${document.getElementById("text").clientHeight + document.getElementById("Scroller").clientHeight}px`
+    // document.getElementById("Mv").style.height = `${document.getElementById("text").clientHeight + document.getElementById("Scroller").clientHeight}px`;
+    document.getElementById("Scroller").style.overflow = "hidden";
+    this.setState({scrollTop: document.getElementById("text").getBoundingClientRect().bottom});
+  }
+  onScroller(num){
+    this.setState({scrollTop: num});
+    if(num ===  0 ){
+      if(this.state.pagePos === false){
+        document.getElementById("Mv").style.overflow  = "hidden";
+      }else{
+        document.getElementById("Mv").style.overflow  = "scroll";
+      }
+      // document.getElementById("Mv").style.backgroundAttachment = "fixed";
+      document.getElementById("Scroller").style.overflow = "auto";
+    }else if (num >= 1){
+      // document.getElementById("Mv").style.backgroundAttachment = "fixed";
+      this.setState({pagePos: false});
+    }else if(num <= -1){
+      // document.getElementById("Mv").style.overflow  = "hidden";
+      this.setState({pagePos: true});
+      console.log("TEXT");
+    }
+  }
+  render(){
+    var flags;
+    if(this.state.pagePos === false){
+      flags = (
+          <h2>{this.state.scrollTop},FALSE!</h2>
+        )
+    }else{
+      flags = (
+        <div>
+          <h2>{this.state.scrollTop},TRUE!</h2>
+        </div>
+        )
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <div className="text-wrapper" >
+          <div className="scroller" onScroll={()=>{this.onScroller(document.getElementById("text").getBoundingClientRect().bottom - document.getElementById("Scroller").clientHeight)}} id="Scroller">
+            <div class="mv"id="Mv">
+              <p id="text">samplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesampleamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamplesamp</p>
+            </div>
+            <h1 id="ttl">SAMPLE</h1>
+            <p className="contant">ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー<br/>
+            ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー<br/>
+            ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
+            </p>
+          </div>
+        </div>
+        <h2>{flags}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
